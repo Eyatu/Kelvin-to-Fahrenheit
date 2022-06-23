ca2cbf2a8813c1739c515debfd2f7eef77286a47
DemirB <demir.beynam@solvoyo.com>
2022-06-23 13:36:15 +0300

    A simple tool for converting kelvin to fahrenheit

---
+++ kelvin_to_fahrenheit.js
@@ -0,0 +1,10 @@
+//Created a constant, kelvin, which has been set to 293
+const kelvin= 0
+//Created a variable, celsius, which has been set to 273 less than kelvin
+let celsius= kelvin - 273
+//Created a variable, celsius, which has been set accordingly
+let fahrenheit= celsius * (9/5) + 32
+//Rounded down fahrenheit
+fahrenheit= Math.floor(fahrenheit)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+
